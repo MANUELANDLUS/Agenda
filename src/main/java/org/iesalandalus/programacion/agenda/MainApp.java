861fcca5ebf67ab4f60c603b9e859c2d8b90ec3a
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-12-08 19:49:35 +0100

    Clase Agenda y atributos

--- src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
@@ -7,6 +7,22 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("Programa para gestionar una agenda de contactos");
+		String nombre = "  buenos dias como estamos";
+		String iniciales = "";
+		int indice = 0;
+		if (nombre.charAt(indice) != ' ') {
+			iniciales = iniciales + nombre.charAt(indice);
+		}
+		indice = nombre.indexOf(" ");
+		while (indice != -1) {
+			if (nombre.charAt(indice + 1) != ' ') {
+				iniciales = iniciales + nombre.charAt(indice + 1);
+			}
+			indice = nombre.indexOf(" ", indice + 1);
+
+		}
+
+		System.out.println(iniciales.toUpperCase());
 	}
-	
+
 }
