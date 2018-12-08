bf43da02ef1f4a5e66c86f06de11f2bc6b39a239
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-12-08 14:35:45 +0100

    Creo los métodos get y set
    Los metodos set deben cumplir las condiciones descritas en el ejercicio
    4

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -7,4 +7,48 @@ public class Contacto {
 	private String telefono;
 	private String correo;
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		if (nombre != null && nombre != "") {
+			this.nombre = nombre;
+		} else {
+			throw new IllegalArgumentException("El nombre de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		if (telefono != null && telefono != "") {
+			if (telefono.matches(ER_TELEFONO)) {
+				this.telefono = telefono;
+			} else {
+				throw new IllegalArgumentException("El teléfono no tiene un formato válido.");
+			}
+		}else {
+			throw new IllegalArgumentException("El teléfono de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		if (correo != null && correo != "") {
+			if (correo.matches(ER_CORREO)) {
+				this.correo = correo;
+			} else {
+				throw new IllegalArgumentException("El correo no tiene un formato válido.");
+			}
+		}else {
+			throw new IllegalArgumentException("El correo de un contacto no puede ser nulo o vacío.");
+		}
+	}
+
 }
