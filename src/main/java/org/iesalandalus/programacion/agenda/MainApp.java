cb61e6aba711383d4e3332fb99e406d8cc7d0d54
MANUEL SERNA GARCIA <MANUEL81SG@GMAIL.COM>
2018-12-09 21:25:52 +0100

    Metodo Main que nos muestra el menú
    podemos añadir contactos a la agenda, el resto de opciones sin hacer.

--- src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/agenda/MainApp.java
@@ -1,28 +1,88 @@
 package org.iesalandalus.programacion.agenda;
 
 import javax.naming.OperationNotSupportedException;
+
 import org.iesalandalus.programacion.utilidades.Entrada;
 
 public class MainApp {
+	private static final String ERROR = "No funciona";
+	private static final String EXITO = "Funciona!";
 
 	public static void main(String[] args) {
 		System.out.println("Programa para gestionar una agenda de contactos");
-		String nombre = "  buenos dias como estamos";
-		String iniciales = "";
-		int indice = 0;
-		if (nombre.charAt(indice) != ' ') {
-			iniciales = iniciales + nombre.charAt(indice);
-		}
-		indice = nombre.indexOf(" ");
-		while (indice != -1) {
-			if (nombre.charAt(indice + 1) != ' ') {
-				iniciales = iniciales + nombre.charAt(indice + 1);
-			}
-			indice = nombre.indexOf(" ", indice + 1);
 
-		}
+		Agenda agenda;
 
-		System.out.println(iniciales.toUpperCase());
+		agenda = new Agenda();
+		mostrarMenu();
+
+	}
+
+	private static void mostrarMenu() {
+		int opcion;
+		do {
+			System.out.println("MENU PRINCIPAL");
+			System.out.println("1 - Añadir contacto");
+			System.out.println("2 - Buscar contacto");
+			System.out.println("3 - Borrar contacto");
+			System.out.println("4 - Listar agenda");
+			System.out.println("5 - Salir");
+			opcion = elegirOpcion();
+		} while (opcion < 1 || opcion > 5);
+		ejecutarOpcion(opcion);
+	}
+
+	private static int elegirOpcion() {
+		int opcion;
+		opcion = Entrada.entero();
+		return opcion;
+	}
+
+	private static void ejecutarOpcion(int opcion) {
+		switch (opcion) {
+		case 1:
+			anadirContacto();
+			mostrarMenu();
+			break;
+		case 2:
+
+			mostrarMenu();
+			break;
+		case 3:
+
+			mostrarMenu();
+			break;
+		case 4:
+
+			mostrarMenu();
+			break;
+		case 5:
+			break;
+		}
+	}
+
+	private static void anadirContacto() {
+		String nombre;
+		String telefono;
+		String correo;
+		Contacto contacto;
+
+		do {
+			System.out.println("Nombre?");
+			nombre = Entrada.cadena();
+		} while (nombre == "");
+
+		do {
+			System.out.println("Teléfono?");
+			telefono = Entrada.cadena();
+		} while (telefono == "");
+
+		do {
+			System.out.println("e-mail?");
+			correo = Entrada.cadena();
+		} while (correo == "");
+
+		contacto = new Contacto(nombre, telefono, correo);
 	}
 
 }
